96e7ef9e12c7b0a148df92eecd928b1d7e7cb49a
kanu-e <chukwuemekakanu@gmail.com>
2020-12-09 12:04:09 -0600

    seeds

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,4 @@
 class Player < ApplicationRecord
+    has_many :appearances
+    has_many :games, through: :appearances
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require 'faker'
+
+Player.destroy_all 
+Appearance.destroy_all 
+Game.destroy_all 
+
+Game.create(name: "vs ABC United")
+
+15.times do
+Player.create(name: Faker::Name.name, number: Faker::Number.unique.between(from: 1, to: 20))
+end
