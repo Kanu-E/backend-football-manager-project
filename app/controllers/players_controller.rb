4409046f995b1844ce3c4a88e5b779660a71d7e0
kanu-e <chukwuemekakanu@gmail.com>
2020-12-12 22:52:07 -0600

    save player to database

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -3,4 +3,17 @@ class PlayersController < ApplicationController
         players = Player.all
         render json: players 
     end
+
+    def create
+        player = Player.create(player_params)
+        render json: player
+
+    end
+
+    private
+
+    def player_params
+        params.require(:player).permit(:name, :number)
+    end
+
 end
