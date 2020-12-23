3c69b7350102449fbbcbe3b46ca811b89aacaa58
kanu-e <chukwuemekakanu@gmail.com>
2020-12-23 11:52:27 -0600

    set default values for player_slots

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,18 @@
 class Game < ApplicationRecord
     has_many :rosters
     has_many :players, through: :rosters
+
+    # def initialize(player_1: "player_1", player_2: "player_2", player_3: "player_3", player_4: "player_4", player_5: "player_5", player_6: "player_6", player_7: "player_7", player_8: "player_8", player_9: "player_9", player_10: "player_10", player_11: "player_11" )
+    #     @player_1 = player_1
+    #     @player_2 = player_2
+    #     @player_3 = player_3
+    #     @player_4 = player_4
+    #     @player_5 = player_5
+    #     @player_6 = player_6
+    #     @player_7 = player_7
+    #     @player_8 = player_8
+    #     @player_9 = player_9
+    #     @player_10 = player_10
+    #     @player_11 = player_11
+    #   end
 end
