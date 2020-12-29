0d9d5b964380d2624bc5d5abce0455cb7cdeaadc
kanu-e <chukwuemekakanu@gmail.com>
2020-12-28 22:49:46 -0600

    set up rosters update

--- app/controllers/rosters_controller.rb
+++ app/controllers/rosters_controller.rb
@@ -1,5 +1,17 @@
 class RostersController < ApplicationController
-    def index
-        render json: RosterSerializer.new(Roster.all).serializable_hash[:data].map{|roster| roster[:attributes]} 
+    def update
+        
+        if x = Roster.find(params[:id])
+           x.update(roster_params)
+        else
+            render create
+        end
+    end
+
+
+    private
+
+    def roster_params
+        params.require(:roster).permit(:game_id, :player_id, :position)
     end
 end
